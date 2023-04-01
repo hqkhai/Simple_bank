d2f490fec8a0804cc4af1cb99736c1a2bd062d64
hqkhai <93475167+hqkhai@users.noreply.github.com>
2023-04-01 18:00:02 +0700

    add user

--- Makefile
+++ Makefile
@@ -14,10 +14,21 @@ migratedown:
 	migrate -path db/migration -database "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down
 
 sqlc:
-	docker run --rm -v $(pwd):/src -w /src kjconroy/sqlc generate
+	docker run --rm -v C:\Users\9A312\OneDrive' - 'VNU-HCMUS\Documents\Nam3\HKII\SimpleBank:/src -w /src kjconroy/sqlc generate
 
 test:
 	go test -v -cover ./...
 
+server:
+	go run main.go
 
-.PHONENY: createdb dropdb postgres migrateup migratedown sqlc test
+mock: 
+	mockgen -destination db/mock/store.go simplebank/db/sqlc Store   
+
+migratedown1:
+	migrate -path db/migration -database "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down 1
+
+migrateup1:
+	migrate -path db/migration -database "postgres://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose up 1
+
+.PHONENY: createdb dropdb postgres migrateup migratedown sqlc test server mock migratedown1 migrateup1
--- api/server.go
+++ api/server.go
@@ -1,8 +1,41 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	db "simplebank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
 
 type Server struct {
-	//store  *db.store
+	store  db.Store
 	router *gin.Engine
+}
+
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+	// TODO: add routes to router
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.POST("/transfers", server.createTransfer)
+
+	server.router = router
+	return server
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
 }
